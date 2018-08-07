16decc0129a94a94771c4b6fe65166c2587c12c6
Akash Kava <39438041+ackava@users.noreply.github.com>
2018-08-07 14:11:22 -0400

    wip

---
+++ src/xf/views/AppHost.ts
@@ -0,0 +1,45 @@
+// tslint:disable
+        import { AtomXFControl } from "web-atoms-core/dist/xf/controls/AtomXFControl";
+            export default class Root extends AtomXFControl {
+
+                protected create(): void {
+                    super.create();
+
+                    this.loadXaml(`	<ContentPage xmlns:x="http://schemas.microsoft.com/winfx/2009/xaml" xmlns="http://xamarin.com/schemas/2014/forms">
+	  
+	  <Grid>
+	    
+	    <Grid.RowDefinitions>
+	      
+	      <RowDefinition/>
+	      
+	      <RowDefinition/>
+	      
+	      <RowDefinition/>
+	      
+	    </Grid.RowDefinitions>
+	    
+	    <Grid.ColumnDefinitions>
+	      
+	      <ColumnDefinition/>
+	      
+	      <ColumnDefinition/>
+	      
+	      <ColumnDefinition/>
+	      
+	    </Grid.ColumnDefinitions>
+	    
+	    <Label Text="Sample"/>
+	    
+	    <Label Grid.Row="1" Grid.Column="1"/>
+	    
+	  </Grid>
+	  
+	</ContentPage>`);
+
+                    
+                }
+            }
+
+            
+        
