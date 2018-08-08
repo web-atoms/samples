b2e21f9104e2bfbb51349cdf0a8e372a86d0f8c0
Akash Kava <39438041+ackava@users.noreply.github.com>
2018-08-08 00:10:08 -0400

    wip

---
+++ src/xf/index.js
@@ -0,0 +1,13 @@
+/// <reference path="../../node_modules/web-atoms-amd-loader/umd.js"/>
+
+UMD.map("reflect-metadata","https://cdn.jsdelivr.net/npm/reflect-metadata@0.1.12/Reflect.js");
+UMD.map("web-atoms-core","https://cdn.jsdelivr.net/npm/web-atoms-core@1.0.292/");
+UMD.map("web-atoms-samples","https://cdn.jsdelivr.net/npm/web-atoms-samples@1.0.5/");
+UMD.loadView(
+    "web-atoms-samples/dist/xf/views/AppHost",
+    true,
+    "web-atoms-core/dist/xf/XFApp")
+    .then(function() {})
+    .catch(function(e) {
+        console.error(e);
+    });
