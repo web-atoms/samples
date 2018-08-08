1fcf885aa6ee7f5a7ca087517a9eba3bde5154a0
Akash Kava <39438041+ackava@users.noreply.github.com>
2018-08-08 00:09:53 -0400

    Xamarin sample work in progress

--- src/xf/views/AppHost.ts
+++ src/xf/views/AppHost.ts
@@ -1,50 +1,77 @@
 // tslint:disable
         import { AtomXFControl } from "web-atoms-core/dist/xf/controls/AtomXFControl";
-            export default class Root extends AtomXFControl {
+            
+
+        import {AppHostViewModel} from "../../view-models/AppHostViewModel";
+
+        export default class Root extends AtomXFControl {
 
                 protected create(): void {
                     super.create();
 
-                    this.loadXaml(`	<ContentPage xmlns:x="http://schemas.microsoft.com/winfx/2009/xaml" xmlns="http://xamarin.com/schemas/2014/forms">
+                    this.loadXaml(`	<MasterDetailPage xmlns="http://xamarin.com/schemas/2014/forms" x:Name="e2">
 	  
-	  <Grid>
+	  <MasterDetailPage.Master>
 	    
-	    <Grid.RowDefinitions>
+	    <ContentPage>
 	      
-	      <RowDefinition/>
+	      <ListView x:Name="e1">
+	        
+	        
+	      </ListView>
 	      
-	      <RowDefinition/>
-	      
-	      <RowDefinition/>
-	      
-	    </Grid.RowDefinitions>
+	    </ContentPage>
 	    
-	    <Grid.ColumnDefinitions>
-	      
-	      <ColumnDefinition/>
-	      
-	      <ColumnDefinition/>
-	      
-	      <ColumnDefinition/>
-	      
-	    </Grid.ColumnDefinitions>
-	    
-	    <Label Text="Sample"/>
-	    
-	    <Label Grid.Row="1" Grid.Column="1" x:Name="e1"/>
-	    
-	  </Grid>
+	  </MasterDetailPage.Master>
 	  
-	</ContentPage>`);
+	  <MasterDetailPage.Detail>
+	    
+	    <ContentPage>
+	      
+	      <Label Text="Loading..."/>
+	      
+	    </ContentPage>
+	    
+	  </MasterDetailPage.Detail>
+	  
+	</MasterDetailPage>`);
 
                     
             const e1 = this.find("e1");
             
-            this.runAfterInit( () =>
-            this.setLocalValue(e1, "Text", (this.viewModel.title)) );
+            this.setTemplate(e1, "ItemTemplate", () => new (Root_e1_Creator(this))(this.app));
+            
+
+            this.bind(e1, "ItemsSource",  [["viewModel","menuService","menus"]], false , (v1) => (v1) );
+
+
+            const e2 = this.find("e2");
+            
+                this.setLocalValue(e2, "viewModel",  this.resolve(AppHostViewModel) );
 
                 }
             }
 
             
+// template
+function Root_e1_Creator(__creator: any): any {
+    return class Root_e1 extends AtomXFControl {
+
+                protected create(): void {
+                    super.create();
+
+                    this.loadXaml(`	<Label x:Name="e1"/>`);
+
+                    
+            const e1 = this.find("e1");
+            
+            this.runAfterInit( () =>
+            this.setLocalValue(e1, "Text", (this.data.label)) );
+
+                }
+            }
+
+            
+        ;
+}
         
