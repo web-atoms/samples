421c3f6e1880a0a84714c8659967cedc14964369
Akash Kava <39438041+ackava@users.noreply.github.com>
2021-11-30 21:10:54 +0530

    vb

---
+++ .vscode/.waSetup.js
@@ -0,0 +1,13 @@
+// Add Font awesome
+const fa = document.createElement("link");
+fa.href = "https://npm-git.azureedge.net/npm/package/@c8private/fa-icons@5.15.4/css/all.min.css";
+fa.crossOrigin = "anonymous";
+fa.rel = "stylesheet";
+document.head.appendChild(fa);
+
+// Bootstrap
+const bs = document.createElement("link");
+bs.href = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css";
+bs.crossOrigin = "anonymous";
+bs.rel = "stylesheet";
+document.head.appendChild(bs);
